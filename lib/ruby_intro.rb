602c995d1707b95fd1dfa3fab229c2d78c7e2ae0
EC2 Default User <ec2-user@ip-172-31-30-184.us-east-2.compute.internal>
2018-09-14 06:33:40 +0000

    homework1 completed with test log file

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,62 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  total = 0
+  arr.each {|a| total += a}
+  total
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  if arr.length == 0
+    0
+  elsif arr.length == 1
+    arr[0]
+  else
+    arr1 = arr.sort
+    arr1[-2] + arr1[-1]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length >= 2
+    for i in 0..arr.length-1
+      for j in i+1..arr.length-1
+        if arr[i] + arr[j] == n
+          return true
+        end
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s[0] =~ /[a-z]/i && s[0] =~ /[^aeiou]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s.empty? || s =~ /[^01]/
+  s == "0" || s[-2, 2] == "00"
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError.new("Illegal argument") if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  attr_reader :isbn, :price
+  attr_writer :isbn, :price
+  def price_as_string
+    "$%.2f" % [price]
+  end
 end
